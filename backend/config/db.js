a26113d05f79cba91969a73cfb38f16c043f4a07
Brandon-Knorr <bknorr@my.wctc.edu>
2025-11-19 17:39:42 -0600

    fix(server.js, db.js): moved the intializeDatabase function from the server to the db.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,23 @@
-import {neon} from '@neondatabase/serverless';
+import { neon } from '@neondatabase/serverless';
 import "dotenv/config";
 
 // creates a sql connection using the DB URL from the .env file
 export const sql = neon(process.env.DATABASE_URL);
+
+export async function initializeDatabase() {
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10, 2) NOT NULL,
+            category VARCHAR(100),
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`;
+
+        console.log("Database initialized SUCCESSFULLY");
+    } catch (error) {
+        console.log("Database initialization FAILED:", error.message);
+        process.exit(1); // status code 1 means failure 0 means success
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import dotenv, { parse } from 'dotenv';
-import { sql } from './config/db.js';
+import dotenv from 'dotenv';
+import { initializeDatabase } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import transactionsRoute from './routes/transactionsRoute.js';
 
@@ -14,24 +14,6 @@ app.use(express.json());
 app.use(rateLimiter);
 
 const PORT = process.env.PORT || 5001;
-
-async function initializeDatabase() {
-  try {
-    await sql`CREATE TABLE IF NOT EXISTS transactions(
-      id SERIAL PRIMARY KEY,
-      user_id VARCHAR(255) NOT NULL,
-      title VARCHAR(255) NOT NULL,
-      amount DECIMAL(10, 2) NOT NULL,
-      category VARCHAR(100),
-      created_at DATE NOT NULL DEFAULT CURRENT_DATE
-    )`;
-
-    console.log("Database initialized SUCCESSFULLY");
-  } catch (error) {
-      console.log("Database initialization FAILED:", error.message);
-      process.exit(1); // status code 1 means failure 0 means success
-  }
-};
 
 app.get("/health", (req, res) => {
   res.send("Welcome to the Transactions API");
