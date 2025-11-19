0a2f23f09429fe8673ade7ea7aa6d6e44c95eff5
Brandon-Knorr <bknorr@my.wctc.edu>
2025-11-19 17:25:14 -0600

    fix(transactionRoute.js, transactionController.js): imported sql from the config in controller. removed export from the routes and fixed endpoint of the summary.

--- backend/routes/transactionsRoute.js
+++ backend/routes/transactionsRoute.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { sql } from '../config/db.js';
 import { createTransaction, deleteTransaction, getTransactionsByUserId, getTransactionSummaryByUserId } from '../controllers/transactionController.js';
 
 const router = express.Router();
@@ -10,6 +9,6 @@ router.post("/", createTransaction);
 
 router.delete("/:id", deleteTransaction);
 
-router.get("/api/transactions/summary/:userId", getTransactionSummaryByUserId);
+router.get("/summary/:userId", getTransactionSummaryByUserId);
 
 export default router;
