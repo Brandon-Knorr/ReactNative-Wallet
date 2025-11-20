2448487ad819e3bf96001a46dfb6f69c9fcd0564
Brandon-Knorr <bknorr@my.wctc.edu>
2025-11-19 18:11:06 -0600

    fix(transactionRoute.js, transactionController.js) fixed the import paths after the refactor.

--- backend/src/routes/transactionsRoute.js
+++ backend/src/routes/transactionsRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTransaction, deleteTransaction, getTransactionsByUserId, getTransactionSummaryByUserId } from '../../controllers/transactionController.js';
+import { getTransactionsByUserId, createTransaction, deleteTransaction, getTransactionSummaryByUserId } from '../controllers/transactionController.js';
 
 const router = express.Router();
 
