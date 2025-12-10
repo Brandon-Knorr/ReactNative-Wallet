b6a185789a6077dfef168fda23f7a72b277f3190
Brandon-Knorr <bknorr@my.wctc.edu>
2025-12-10 12:14:26 -0600

    feat!: done final images and completed

--- mobile/app/(auth)/sign-in.jsx
+++ mobile/app/(auth)/sign-in.jsx
@@ -7,96 +7,104 @@ import { styles } from "@/assets/styles/auth.styles";
 import { Ionicons } from "@expo/vector-icons";
 import { COLORS } from "@/constants/colors";
 
-export default function Page() {
+export default function Page ()
+{
   const { signIn, setActive, isLoaded } = useSignIn();
   const router = useRouter();
 
-  const [emailAddress, setEmailAddress] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [ emailAddress, setEmailAddress ] = useState( "" );
+  const [ password, setPassword ] = useState( "" );
+  const [ error, setError ] = useState( "" );
 
   // Handle the submission of the sign-in form
-  const onSignInPress = async () => {
-    if (!isLoaded) return;
+  const onSignInPress = async () =>
+  {
+    if ( !isLoaded ) return;
 
     // Start the sign-in process using the email and password provided
-    try {
-      const signInAttempt = await signIn.create({
+    try
+    {
+      const signInAttempt = await signIn.create( {
         identifier: emailAddress,
         password,
-      });
+      } );
 
       // If sign-in process is complete, set the created session as active
       // and redirect the user
-      if (signInAttempt.status === "complete") {
-        await setActive({ session: signInAttempt.createdSessionId });
-        router.replace("/");
-      } else {
+      if ( signInAttempt.status === "complete" )
+      {
+        await setActive( { session: signInAttempt.createdSessionId } );
+        router.replace( "/" );
+      } else
+      {
         // If the status isn't complete, check why. User might need to
         // complete further steps.
-        console.error(JSON.stringify(signInAttempt, null, 2));
+        console.error( JSON.stringify( signInAttempt, null, 2 ) );
       }
-    } catch (err) {
-      if (err?.errors[0]?.code === "form_password_incorrect") {
-        setError("Incorrect password. Please try again.");
-      } else {
-        setError("An unexpected error occurred. Please try again.");
+    } catch ( err )
+    {
+      if ( err?.errors[ 0 ]?.code === "form_password_incorrect" )
+      {
+        setError( "Incorrect password. Please try again." );
+      } else
+      {
+        setError( "An unexpected error occurred. Please try again." );
       }
     }
   };
 
   return (
     <KeyboardAwareScrollView
-      style={{ flex: 1 }}
-      contentContainerStyle={{ flexGrow: 1 }}
-      enableOnAndroid={true}
-      enableAutomaticScroll={true}
-      extraScrollHeight={50}
+      style={ { flex: 1 } }
+      contentContainerStyle={ { flexGrow: 1 } }
+      enableOnAndroid={ true }
+      enableAutomaticScroll={ true }
+      extraScrollHeight={ 50 }
     >
-      <View style={styles.container}>
+      <View style={ styles.container }>
         <Image
-          source={require("../../assets/images/robotv1.png")}
-          style={styles.illustration}
+          source={ require( "../../assets/images/robotv2.png" ) }
+          style={ styles.illustration }
         />
-        <Text style={styles.title}>Welcome Back</Text>
+        <Text style={ styles.title }>Welcome Back</Text>
 
-        {error ? (
-          <View style={styles.errorBox}>
-            <Ionicons name="alert-circle" size={20} color={COLORS.expense} />
-            <Text style={styles.errorText}>{"Something went wrong!"}</Text>
-            <TouchableOpacity onPress={() => setError("")}>
-              <Ionicons name="close" size={20} color={COLORS.textLight} />
+        { error ? (
+          <View style={ styles.errorBox }>
+            <Ionicons name="alert-circle" size={ 20 } color={ COLORS.expense } />
+            <Text style={ styles.errorText }>{ "Something went wrong!" }</Text>
+            <TouchableOpacity onPress={ () => setError( "" ) }>
+              <Ionicons name="close" size={ 20 } color={ COLORS.textLight } />
             </TouchableOpacity>
           </View>
-        ) : null}
+        ) : null }
 
         <TextInput
-          style={[styles.input, error && styles.errorInput]}
+          style={ [ styles.input, error && styles.errorInput ] }
           autoCapitalize="none"
-          value={emailAddress}
-          placeholderTextColor={"#9A8478"}
+          value={ emailAddress }
+          placeholderTextColor={ "#9A8478" }
           placeholder="Enter email"
-          onChangeText={(email) => setEmailAddress(email)}
+          onChangeText={ ( email ) => setEmailAddress( email ) }
         />
 
         <TextInput
-          style={[styles.input, error && styles.errorInput]}
-          value={password}
-          placeholderTextColor={"#9A8478"}
+          style={ [ styles.input, error && styles.errorInput ] }
+          value={ password }
+          placeholderTextColor={ "#9A8478" }
           placeholder="Enter password"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
+          secureTextEntry={ true }
+          onChangeText={ ( password ) => setPassword( password ) }
         />
 
-        <TouchableOpacity style={styles.button} onPress={onSignInPress}>
-          <Text style={styles.buttonText}>Sign In</Text>
+        <TouchableOpacity style={ styles.button } onPress={ onSignInPress }>
+          <Text style={ styles.buttonText }>Sign In</Text>
         </TouchableOpacity>
 
-        <View style={styles.footerContainer}>
-          <Text style={styles.footerText}>Don&apos;t have an account?</Text>
+        <View style={ styles.footerContainer }>
+          <Text style={ styles.footerText }>Don&apos;t have an account?</Text>
           <Link href="/sign-up" asChild>
-            <TouchableOpacity onPress={() => router.back()}>
-              <Text style={styles.linkText}>Sign Up</Text>
+            <TouchableOpacity onPress={ () => router.back() }>
+              <Text style={ styles.linkText }>Sign Up</Text>
             </TouchableOpacity>
           </Link>
         </View>
